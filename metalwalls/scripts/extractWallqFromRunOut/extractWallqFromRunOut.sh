e9c9a0a41c301b4f4f5a894614112ecc07d1186d
KonradBreitsprecher <konradbreitsprecher@gmail.com>
2015-04-23 11:31:04 +0200

    electrode stl files

--- metalwalls/scripts/extractWallqFromRunOut/extractWallqFromRunOut.sh
+++ metalwalls/scripts/extractWallqFromRunOut/extractWallqFromRunOut.sh
@@ -1,12 +1,20 @@
-cd $HOME/data/EQUI_CP/
+cd /work/konrad/POROUS/hornetData/PRODUCTION/
 
-for d in */3 ; do
-	fname="${d:0: ${#d}-2}_wallq"
-	echo $fname
-	if [[ $fname == *"_0ACN"* ]]
-	then
-		grep "Total charge wall species  5" $d/run.out | awk '{print $6/3821}' >> $fname
-	else
-		grep "Total charge wall species  8" $d/run.out | awk '{print $6/3821}' >> $fname
-	fi
+for syst in */ ; do
+	av_wallq=${syst}av_wallq.out
+	rm $av_wallq
+	for run in 6 7 8; do
+		d=$syst$run
+		fname="$d/wall_q_from_run.out"
+		rm $fname
+		echo $fname
+		if [[ $fname == *"_0ACN"* ]]
+		then
+			grep "Total charge wall species  5" $d/run.out | awk '{print $6/3821}' >> $fname
+		else
+			grep "Total charge wall species  8" $d/run.out | awk '{print $6/3821}' >> $fname
+		fi
+		cat $fname >> $av_wallq
+	done
+	echo "Written $av_wallq"
 done
